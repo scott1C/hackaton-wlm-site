e7b1a767b326fb5e3b5fb7393f39646fd55d4434
Toxic_Blade <toxicblade.work@gmail.com>
2023-11-25 06:39:56 +0200

    base headers changed

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,17 +1,33 @@
 import React, { useState, useEffect } from "react";
-import { DataGrid } from "@mui/x-data-grid";
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
 
 const columns = [
 	{ field: "id", headerName: "ID", width: 90 },
 	{ field: "age", headerName: "Age", width: 150 },
 	{ field: "mother", headerName: "Mother", width: 150 },
 	{ field: "father", headerName: "Father", width: 110 },
+	{ field: "welfare", headerName: "Welfare", width: 160, hide: true },
+	{ field: "smoking", headerName: "Smoking", width: 160, hide: true },
+	{ field: "drugs", headerName: "Drugs", width: 160, hide: true },
+	{ field: "drinking", headerName: "Drinking", width: 160, hide: true },
 	{ field: "sex", headerName: "Sex", width: 160 },
 	{ field: "result", headerName: "Result", width: 160 },
 ];
 
 const DataGridDemo = () => {
 	const [data, setData] = useState([]);
+	const [columnVisibilityModel, setColumnVisibilityModel] = useState({
+		id: true,
+		age: true,
+		welfare: false,
+		smoking: false,
+		drugs: false,
+		drinking: false,
+		mother: true,
+		father: true,
+		sex: true,
+		result: true,
+	});
 
 	useEffect(() => {
 		fetch("http://localhost:5000/api/csvdata") // Replace with your API endpoint
@@ -33,6 +49,13 @@ const DataGridDemo = () => {
 				pageSize={5}
 				checkboxSelection
 				disableRowSelectionOnClick
+				components={{
+					Toolbar: GridToolbar,
+				}}
+				columnVisibilityModel={columnVisibilityModel}
+				onColumnVisibilityModelChange={(newModel) => {
+					setColumnVisibilityModel(newModel);
+				}}
 			/>
 		</div>
 	);
